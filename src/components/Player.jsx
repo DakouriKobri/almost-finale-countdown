b8fab5ea32b20d2ba2124c5e09f64a8fdf6920e5
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-07-27 12:00:43 +0200

    Clear input field

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -7,11 +7,12 @@ export default function Player() {
 
   function handleClick() {
     setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = '';
   }
 
   return (
     <section id="player">
-      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
+      <h2>Welcome, {enteredPlayerName ?? 'unknown entity'}!</h2>
       <p>
         <input ref={playerName} type="text" />
 
