96b55d51d276bf609fdea7a5319297f56b0e6089
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-07-31 07:03:05 +0200

    Add ResultModal

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,40 +1,47 @@
 // NPM Packages
 import { useRef, useState } from 'react';
 
+// Project Imports
+import ResultModal from './ResultModal';
+
 export default function TimerChallenge({ title, targetTime }) {
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
   const timer = useRef();
+  const dialog = useRef();
 
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.showModal();
     }, targetTime * 1000);
 
     setTimerStarted(true);
   }
 
   function handleStop() {
+    dialog.current.showModal();
     clearTimeout(timer.current);
   }
 
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
+    <>
+      <ResultModal ref={dialog} result="lost" targetTime={targetTime} />
 
-      {timerExpired && <p>You lost</p>}
-
-      <p className="challenge-time">
-        {targetTime} second{targetTime > 1 ? 's' : ''}
-      </p>
-      <p>
-        <button onClick={timerStarted ? handleStop : handleStart}>
-          {timerStarted ? 'Stop' : 'Start'} Challenge
-        </button>
-      </p>
-      <p className={timerStarted ? 'active' : undefined}>
-        {timerStarted ? 'Timer is running...' : 'Timer inactive'}
-      </p>
-    </section>
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? 's' : ''}
+        </p>
+        <p>
+          <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? 'Stop' : 'Start'} Challenge
+          </button>
+        </p>
+        <p className={timerStarted ? 'active' : undefined}>
+          {timerStarted ? 'Timer is running...' : 'Timer inactive'}
+        </p>
+      </section>
+    </>
   );
 }
